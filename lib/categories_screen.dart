66c90b73a92b2916cd81d48164bffd766f614f94
Chijioke Chika <8pluscreative@gmail.com>
2022-08-16 14:53:33 +0100

    Building UI

--- lib/categories_screen.dart
+++ lib/categories_screen.dart
@@ -1,12 +1,29 @@
 import 'package:flutter/material.dart';
 
+import './dummy_data.dart';
+import 'category_item.dart';
+
 class CategoriesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return GridView(
-      children: [],
-      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-        maxCrossAxisExtent: 250,
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('SwiftMeal'),
+      ),
+      body: GridView(
+        padding: const EdgeInsets.all(25),
+        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 250,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+        ),
+        children: DUMMY_CATEGORIES
+            .map((mealCategory) => CategoryItem(
+                  mealCategory.title,
+                  mealCategory.color,
+                ))
+            .toList(),
       ),
     );
   }
