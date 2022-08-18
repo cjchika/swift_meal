c81a41a43a37d6dd50465a076d3a50bc4abf2c7c
Chijioke Chika <8pluscreative@gmail.com>
2022-08-18 20:57:35 +0100

    Improving UI

--- lib/screens/categories_screen.dart
+++ lib/screens/categories_screen.dart
@@ -6,26 +6,21 @@ import '../dummy_data.dart';
 class CategoriesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('SwiftMeal'),
+    return GridView(
+      padding: const EdgeInsets.all(25),
+      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+        maxCrossAxisExtent: 250,
+        childAspectRatio: 3 / 2,
+        crossAxisSpacing: 20,
+        mainAxisSpacing: 20,
       ),
-      body: GridView(
-        padding: const EdgeInsets.all(25),
-        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-          maxCrossAxisExtent: 250,
-          childAspectRatio: 3 / 2,
-          crossAxisSpacing: 20,
-          mainAxisSpacing: 20,
-        ),
-        children: DUMMY_CATEGORIES
-            .map((mealCategory) => CategoryItem(
-                  mealCategory.id,
-                  mealCategory.title,
-                  mealCategory.color,
-                ))
-            .toList(),
-      ),
+      children: DUMMY_CATEGORIES
+          .map((mealCategory) => CategoryItem(
+                mealCategory.id,
+                mealCategory.title,
+                mealCategory.color,
+              ))
+          .toList(),
     );
   }
 }
--- lib/screens/tabs_screen.dart
+++ lib/screens/tabs_screen.dart
@@ -10,32 +10,48 @@ class TabsScreen extends StatefulWidget {
 }
 
 class _TabsScreenState extends State<TabsScreen> {
+  final List<Map<String, Object>> _pages = [
+    {
+      'page': CategoriesScreen(),
+      'title': 'Categories',
+    },
+    {
+      'page': FavouritesScreen(),
+      'title': 'Favorites',
+    },
+  ];
+
+  int _selectedPageIndex = 0;
+
+  void _selectPage(int index) {
+    setState(() {
+      _selectedPageIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 2,
-      child: Scaffold(
-        appBar: AppBar(
-          title: Text('Meals'),
-          bottom: TabBar(tabs: [
-            Tab(
-              icon: Icon(
-                Icons.category,
-              ),
-              text: 'Categories',
-            ),
-            Tab(
-              icon: Icon(
-                Icons.favorite,
-              ),
-              text: 'Favourites',
-            ),
-          ]),
-        ),
-        body: TabBarView(children: [
-          CategoriesScreen(),
-          FavouritesScreen(),
-        ]),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(_pages[_selectedPageIndex]['title']),
+      ),
+      body: _pages[_selectedPageIndex]['page'],
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: _selectPage,
+        backgroundColor: Colors.indigo,
+        unselectedItemColor: Colors.white,
+        selectedItemColor: Colors.limeAccent,
+        currentIndex: _selectedPageIndex,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.category),
+            label: 'Categories',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.favorite),
+            label: 'Favorites',
+          ),
+        ],
       ),
     );
   }
