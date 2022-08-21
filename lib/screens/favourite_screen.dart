82f781889b3f1282dea086937e1fb4bb2fb2db89
Chijioke Chika <8pluscreative@gmail.com>
2022-08-21 15:37:21 +0100

    Improving UI/UX

--- lib/screens/favourite_screen.dart
+++ lib/screens/favourite_screen.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
+import '../models/meal.dart';
 
 class FavouritesScreen extends StatelessWidget {
-  const FavouritesScreen({Key key}) : super(key: key);
+  final List<Meal> favouriteMeals;
+
+  FavouritesScreen(this.favouriteMeals);
 
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Text('The Favourites'),
+      child: Text('No favourites - add now.'),
     );
   }
 }
